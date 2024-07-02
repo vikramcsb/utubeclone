2c280266686f2ff4bc7473215762fe02e84d0583
vikram jaat <128956207+vikramcsb@users.noreply.github.com>
2024-07-02 10:08:39 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,4 +1,8 @@
 export const API_KEY = 'AIzaSyCMlm7IpQSEkzrDNjaQNZmSAfy5bZj67vU';
+export const API_KEY1 = 'AIzaSyA3XE5bmXAFDHmUKuW70wHG2tSppjQJoLo';
+export const API_KEY2 = 'AIzaSyBF9Tk6tE7MWIHJRucwIGNppFZ1y-8zdkw';
+export const API_KEY3 = 'AIzaSyBMXuuS9XfwlYllhKmwwfTktjkZPXh6eJY';
+export const API_KEY4 = 'AIzaSyC1FHD1mRvPCytuIv6Umuur5aKZce35QiY';
 
 export const value_converter = (value) =>{
       if(value>=1000000){
